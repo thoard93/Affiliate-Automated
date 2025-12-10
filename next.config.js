b8a6eb6b37078ad7d910e97d861a9563f924d6a4
thoard93 <thoard93@gmail.com>
2025-12-10 11:22:46 -0500

    Remove image remote patterns and experimental settings

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'p16-oec-va.ibyteimg.com', // TikTok product images
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com', // Discord avatars
-      },
-      {
-        protocol: 'https',
-        hostname: 'p16-sign-va.tiktokcdn.com', // TikTok profile images
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
